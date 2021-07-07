6c9908e1662789fce99e64378192887821b6667e
TomatoT1 <86846293+TomatoT1@users.noreply.github.com>
2021-07-07 20:21:40 +0000

    Triangle added

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ def circle():
   for i in range(180):
     turtle.forward(2)
     turtle.left(2)
-
+def triangle():
+  for i in range(3):
+    turtle.left(120)
+    turtle.forward(50)
 if choice == "circle":
   circle()
+if choice == "triangle":
+  triangle()
